9da91ac1903e4fd34564f5f4f40a1c1bbbd09038
cLamberti <150824061+cLamberti@users.noreply.github.com>
2026-03-22 19:25:42 -0600

    initial commit

---
+++ lab1.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+ abstract class Animal {
+    public void makeSound() {
+    System.out.println("El animal hace sonido");
+    }
+    }
+    class Perro extends Animal {
+    @Override
+    public void makeSound() {
+    System.out.println("Wuf wuf");
+    }
+    }
+    class Gato extends Animal {
+    @Override
+    public void makeSound() {
+    System.out.println("Miau");
+    }
+    }
+    class Vaca extends Animal {
+      @Override
+      public void makeSound() {
+      System.out.println("Muuu");
+      }
+      }
+    public class Main {
+    public static void main(String[] args) {
+
+    ArrayList<Animal> Animals = new ArrayList<>();
+    Animals.add(new Vaca());
+    Animals.add(new Perro());
+    Animals.add(new Gato());
+   
+    for (Animal animal : Animals) {
+      animal.makeSound();
+  }
+    }
+    }
